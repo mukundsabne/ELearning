ca3acee034bad84e0e6ad92d7d48064c451e459c
Mukund Sabne <mukundsabne811@gmail.com>
2019-01-03 23:05:16 +0530

    Menu Activation

--- EduLearn/views.py
+++ EduLearn/views.py
@@ -1,19 +1,19 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request,'index.html')
+    return render(request,'index.html',{'initial':'h'})
 
 def aboutus(request):
-    return render(request,'about-us.html')
+    return render(request,'about-us.html',{'initial':'a'})
 
 def services(request):
-    return render(request,'services.html')
+    return render(request,'services.html',{'initial':'s'})
 
 def gallery(request):
-    return render(request,'gallery.html')
+    return render(request,'gallery.html',{'initial':'g'})
 
 def contact(request):
-    return render(request,'contact-us.html')
+    return render(request,'contact-us.html',{'initial':'c'})
 
 def faq(request):
-    return render(request,'faq.html')
+    return render(request,'faq.html',{'initial':'f'})
